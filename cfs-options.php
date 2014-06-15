908638422f1df758759f0546d300f2e6ac9a39ea
Nick Sampsell <nick@nicksampsell.com>
2014-06-15 19:00:06 -0400

    Converted procedural code to initial object oriented setup.

--- cfs-options.php
+++ cfs-options.php
@@ -2,126 +2,144 @@
 /*
 Plugin Name: Custom Field Suite Options Pages
 */
-function create_options_pages()
-{
-	register_post_type('cfs-options',
-		array(
-			'labels' => array(
-				'name' => __('Options'),
-				'singular_name' => __('Options Page'),
-				'menu_name' => 'Options',
-				'name_admin_bar' => 'Options Page',
-				'all_items' => 'Options Pages',
-				'add_new_item' => 'New Options Page',
-				'edit_item' => 'Edit Options Page',
-				'new_item' => 'Options Page'
-			),
-		'public' => true,
-		'has_archive' => false,
-		'menu_position' => 80,
-		'show_in_menu' => false,
-		'supports' => array('title','custom-fields'),
-		'exclude_from_search' => true,
-		'public' => false,
-		'show_ui' => false
-		)
-	);
-}
 
-add_action('init','create_options_pages');
+require_once(plugin_dir_path(__FILE__) . '../custom-field-suite/cfs.php');
 
-function cfs_options($title=null)
+class CFS_Options extends Custom_Field_Suite
 {
-	$page = get_page_by_title($title, 'OBJECT', 'cfs-options');
-	if($page !== NULL)
+	public function __construct($cfs)
 	{
-		return $page->ID;
-	}
-}
-
-function create_cfs_options()
-{
-	add_menu_page('CFS Options','CFS Options','manage_options','cfs-options','cfs_global', '',80); //add url to icon in empty spot
-	add_submenu_page('cfs-options', 'Create Options Page', 'Create New','manage_options','cfs-options-create','cfs_options_create');
-	remove_submenu_page('cfs-options','cfs-options'); //Remove the duplicate CFS Options link.  Will not be used.
-
-	$query = new WP_Query('post_type=cfs-options&orderby=title&order=asc');
-
-	if($query->have_posts()): while($query->have_posts()): $query->the_post();
-		add_submenu_page('cfs-options', get_the_title(), get_the_title(),'manage_options','cfs-options-edit-'.get_the_ID(),'cfs_options_edit');
-	endwhile;endif;
-}
-
-add_action('admin_menu','create_cfs_options');
-
-function cfs_global()
-{
-	echo 'THE_DEFAULT_PAGE';
-}
-
-function cfs_options_create()
-{
-	echo 'CREATE OPTIONS PAGE';
-}
-
-function cfs_options_edit()
-{
-
-	global $cfs;
-
-	$cfs->form->load_assets();
-	$item = array(
-		'post_id' => str_replace('cfs-options-edit-','',$_GET['page'])
-		);
-
-	$page = get_post($item['post_id']);
-
-	echo '<div class="wrapper page wrap">';
-	echo '<h2>'.$page->post_title.'</h2>';
-	echo $cfs->form($item);
-	echo '</div>';
-}
-
-/**
- * Creates an options page programmatically, if one with given title
- * and slug do not already exist.
- * @param  string $title
- * @param  string $slug
- * @return void
- */
-function create_option_page($title, $slug)
-{
-
-	$exists = wp_cache_get('cfs-options-'.$slug);
-	if($exists === false)
-	{
-		$exists = new WP_Query(array('title'=>$title,'name'=>$slug,'post_type'=>'cfs-options'));
-		wp_cache_set('cfs-options-'.$slug, $exists);
+		$this->cfs = $cfs;
+		add_action('init', array($this, 'init'));
+		add_action('init', array($this, 'check_for_updates'));
+		add_action('admin_menu', array($this, 'create_menu'));
 	}
 
-	if(!$exists->have_posts())
+	public function init()
 	{
-		wp_insert_post(array(
-			'post_name' => $slug,
-			'post_title' => $title,
-			'post_type' => 'cfs-options',
-			'post_status' => 'publish'
-		));
+		register_post_type('cfs-options',
+			array(
+				'labels' => array(
+					'name' => __('Options'),
+					'singular_name' => __('Options Page'),
+					'menu_name' => 'Options',
+					'name_admin_bar' => 'Options Page',
+					'all_items' => 'Options Pages',
+					'add_new_item' => 'New Options Page',
+					'edit_item' => 'Edit Options Page',
+					'new_item' => 'Options Page'
+				),
+			'public' => true,
+			'has_archive' => false,
+			'menu_position' => 80,
+			'show_in_menu' => false,
+			'supports' => array('title','custom-fields'),
+			'exclude_from_search' => true,
+			'public' => false,
+			'show_ui' => false
+			)
+		);		
 	}
-}
 
-/**
- * Delete an options page with the given slug.  Only delete is the options page exists.
- * @param  string $slug
- * @return void
- */
-function delete_option_page($slug)
-{
-
-	$page = get_posts(array('name'=>$slug,'post_type'=>'cfs-options'));
-
-	if(!empty($page))
+	public function create_menu()
 	{
-		wp_delete_post($page[0]->ID, true);
+		add_menu_page('CFS Options','CFS Options','manage_options','cfs-options','cfs_global', '',80); //add url to icon in empty spot
+		add_submenu_page('cfs-options', 'Create Options Page', 'Create New','manage_options','cfs-options-create','cfs_options_create');
+		remove_submenu_page('cfs-options','cfs-options'); //Remove the duplicate CFS Options link.  Will not be used.
+
+		$query = new WP_Query('post_type=cfs-options&orderby=title&order=asc');
+
+		if($query->have_posts())
+		{
+			while($query->have_posts())
+			{
+				$query->the_post();
+				add_submenu_page('cfs-options', get_the_title(), get_the_title(),'manage_options','cfs-options-edit-'.get_the_ID(),'cfs_options_edit');
+			}
+		}
 	}
+
+	public function options_create()
+	{
+		echo 'CREATE OPTIONS PAGE';
+	}
+
+	public function options_edit()
+	{
+		global $cfs;
+
+		$cfs->form->load_assets();
+		$item = array(
+			'post_id' => str_replace('cfs-options-edit-','',$_GET['page'])
+			);
+
+		$page = get_post($item['post_id']);
+
+		echo '<div class="wrapper page wrap">';
+		echo '<h2>'.$page->post_title.'</h2>';
+		echo $cfs->form($item);
+		echo '</div>';
+	}
+
+	/**
+	 * Creates an options page programmatically, if one with given title
+	 * and slug do not already exist.
+	 * @param  string $title
+	 * @param  string $slug
+	 * @return void
+	 */
+	function create_option_page($title, $slug)
+	{
+
+		$exists = wp_cache_get('cfs-options-'.$slug);
+		if($exists === false)
+		{
+			$exists = new WP_Query(array('title'=>$title,'name'=>$slug,'post_type'=>'cfs-options'));
+			wp_cache_set('cfs-options-'.$slug, $exists);
+		}
+
+		if(!$exists->have_posts())
+		{
+			wp_insert_post(array(
+				'post_name' => $slug,
+				'post_title' => $title,
+				'post_type' => 'cfs-options',
+				'post_status' => 'publish'
+			));
+		}
+	}
+
+	/**
+	 * Delete an options page with the given slug.  Only delete is the options page exists.
+	 * @param  string $slug
+	 * @return void
+	 */
+	function delete_option_page($slug)
+	{
+
+		$page = get_posts(array('name'=>$slug,'post_type'=>'cfs-options'));
+
+		if(!empty($page))
+		{
+			wp_delete_post($page[0]->ID, true);
+		}
+	}
+
+
+	function cfs_options($title=null)
+	{
+		$page = get_page_by_title($title, 'OBJECT', 'cfs-options');
+		if($page !== NULL)
+		{
+			return $page->ID;
+		}
+	}
+
+	public function check_for_updates()
+	{
+
+	}
+
 }
+global $cfs;
+$cfs_options = new CFS_Options($cfs);
